1f00c97dac46597c26e74cd796c6591e6cf422b3
EmmyRocky <emmyrockyy@gmail.com>
2022-05-23 03:05:24 -0700

    main.h

--- main.h
+++ main.h
@@ -8,8 +8,16 @@
 
 int _printf(const char *format, ...);
 
+/* get_print */
+
+int (*get_print(char s))(va_list, flags_t *);
+
 
 /* write_funcs */
 
 int _putchar(char c);
 int _puts(char *str);
+
+/* get_flag */
+
+int get_flag(char s, flags_t *f);
