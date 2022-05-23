9f9fe4664e7d92f61e44a96584394050fd6bbe6e
EmmyRocky <emmyrockyy@gmail.com>
2022-05-23 04:53:27 -0700

    main.h

--- main.h
+++ main.h
@@ -21,3 +21,13 @@ int _puts(char *str);
 /* get_flag */
 
 int get_flag(char s, flags_t *f);
+
+/* converter */
+
+char *convert(unsigned long int num, int base, int lowercase);
+
+/* print_custom */
+
+int print_rot13(va_list l, flags_t *f);
+int print_rev(va_list l, flags_t *f);
+int print_bigS(va_list l, flags_t *f);
